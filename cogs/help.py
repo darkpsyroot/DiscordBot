f22d21d8c37f5a774eb644da05292e6ca9bd6889
Francisco Reyes <franciscoalonso.reyes94@gmail.com>
2025-08-17 16:28:22 -0600

    Api youtube reestructura comandos

--- cogs/help.py
+++ cogs/help.py
@@ -6,7 +6,7 @@ class CustomHelp(commands.Cog):
 
     @commands.command(name="help")
     async def help(self, ctx):
-        await ctx.send("Escribe `!comandos listacomandos` para ver los comandos disponibles.")
+        await ctx.send("Escribe `!furina lista comandos` para ver los comandos disponibles.")
 
 async def setup(bot):
     await bot.add_cog(CustomHelp(bot))
